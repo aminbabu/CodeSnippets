cd835400180dd5efbbdc6c21e037a4932f63aad8
Amin Babu <amin.babu.bd@gmail.com>
2020-10-19 21:33:15 +0600

    Difference between var, let, and const and arithmetic operator.

---
+++ JavaScript/10.19.2020/01. Arithmetic/js/javaScript.js
@@ -0,0 +1,43 @@
+// JavaScript Arithmetic Operators: +, -, *, /, %, ++, --, **
+// var x = 10;
+// var y = 4;
+
+// Adition
+// var sum = x + y;
+// document.write('X + Y = ' + sum + '<br/>');
+// document.write(`X + Y = ${sum}`);
+
+// Substraction
+// var sub = x - y;
+// document.write('X - Y = ' + sub);
+
+
+// Multiplication
+// var mul = x * y;
+// document.write('X * Y = ' + mul);
+
+
+// Division
+// var division = x / y;
+// document.write('X / Y = ' + division);
+
+
+// Reminder
+// var reminder = x % y;
+// document.write('X % Y = ' + reminder);
+
+
+// Increment
+// var x = 20;
+// x = x + 1;
+// x++;
+// document.write('After increment x is: ' + x);
+
+// decrement
+// x--;  // x = x - 1;
+// document.write('After decrement y is: ' + x);
+
+// Exponentiation
+// x = 20, y = 2;
+// var expo = x ** y;
+// document.write('x ** y = ' + expo);
---
+++ JavaScript/10.19.2020/VariablesKeywords/js/javaScript.js
@@ -0,0 +1,38 @@
+// // Using var keyword
+// var name = 'Siri';
+// console.log(name);
+
+// // redeclair
+// var name = 'John';
+// console.log(name);
+
+// // reuse
+// name = 'Rita';
+// console.log(name);
+
+
+
+// // Using let keyword
+// let name = 'Siri';
+// console.log(name);
+
+// // redeclair
+// // let name = "john";
+// // console.log(name);
+
+// // reuse
+// name = 'Rita';
+// console.log(name);
+
+
+// // Using const keyword
+// const name = 'Siri';
+// console.log(name);
+
+// // redeclair
+// // const name = 'John';
+// // console.log(name);
+
+// // reuse
+// name = 'John';
+// console.log(name);
